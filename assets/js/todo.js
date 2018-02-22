ebd8b934e725f2861c7b157cde8b5309fe741610
blaze-nitdgp <abhishekkarn96@gmail.com>
2018-02-22 17:45:17 +0530

    Added different styling elements and added much more interactivity to the todo list

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,5 +1,5 @@
 
-$("li").click(function(){
+$("ul").on("click","li",function(){
 	if($(this).css("color")==="rgb(128, 128, 128)"){
 		$(this).css("color","black");
 	$(this).css("text-decoration","none");
@@ -11,9 +11,23 @@ $("li").click(function(){
     }
 });
 
-$("span").click(function(event){
+$("ul").on("click","span",function(event){
     $(this).parent().fadeOut(500,function(){
        $(this).remove();	
     })
     event.stopPropagation();
+});
+
+
+$("input[type='text']").keypress(function(){
+	if(event.which==13)
+	{
+		var todoText=$(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todoText + "</li>");
+	}
+});
+
+$(".fa-plus").click(function(){
+  $("input[type='text']").fadeToggle();
 });
